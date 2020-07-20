6e1d415185ca2ddc552024aa9ecfc583af9a8053
mlowak <martyna.lowak@gmail.com>
2020-07-20 09:25:29 +0200

    koniec

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log("Martyna");
+
+let firstNumber = 2;
+let secondNumber = 5;
+
+function dodawanie(firstNumber, secondNumber){
+    let wynik = firstNumber + secondNumber;
+    return wynik
+}
+
+console.log(dodawanie(firstNumber, secondNumber));
